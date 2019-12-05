5aa35edd42db8211daa89182eda383e54f078d54
gabriel <gbp@ic.ufal.br>
2019-12-04 22:59:39 -0300

    adding the model message and configuring apps on settings

--- chatApp/settings.py
+++ chatApp/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # System apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My apps
+    'widget_tweaks',
+    'rest_framework',
+    'chat'
 ]
 
 MIDDLEWARE = [
