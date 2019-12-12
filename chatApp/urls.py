f95716984ce178f3d40db969fb0283caf53026de
gabriel <gbp@ic.ufal.br>
2019-12-12 02:05:45 -0300

    adding icon and link to index on Chart title

--- chatApp/urls.py
+++ chatApp/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic.base import RedirectView
+
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('chat.urls'))
+    path('', include('chat.urls')),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
 ]
--- static/js/chat.js
+++ static/js/chat.js
@@ -10,7 +10,7 @@ let userState = '';
 
 const userDiv = (senderId, receiverId, name, online) =>
     (`<a href="/chat/${senderId}/${receiverId}" id="user${receiverId}" class="collection-item row">
-                    <img src="https://frontend-1.adjust.com/new-assets/images/site-images/interface/user.svg" class="col s2">
+                    <img src="https://cdn1.iconfinder.com/data/icons/user-interface-203/76/icon-24-512.png" class="col s2">
                     <div class="col s10">
                     <span class="title" style="font-weight: bolder">${name}</span>
                     <span style="color: ${online ? 'green' : 'red'}; float: right">${online ? 'online' : 'offline'}</span>
@@ -20,7 +20,7 @@ const userDiv = (senderId, receiverId, name, online) =>
 /* Send takes three args: sender, receiver, message. sender & receiver are ids of users, and message is the text to be sent. */
 function send(sender, receiver, message) {
     //POST to '/api/messages', the data in JSON string format
-    $.post('/api/messages', '{"sender": "'+ sender +'", "receiver": "'+ receiver +'","message": "'+ message +'" }', function (data) {
+    $.post('/api/messages/', '{"sender": "'+ sender +'", "receiver": "'+ receiver +'","message": "'+ message +'" }', function (data) {
         var box = text_box.replace('{sender}', "You"); // Replace the text '{sender}' with 'You'
         box = box.replace('{message}', message); // Replace the text '{message}' with the message that has been sent.
         $('#board').append(box); // Render the message inside the chat-box by appending it at the end.
@@ -32,7 +32,6 @@ function send(sender, receiver, message) {
 function receive() {
     // 'sender_id' and 'receiver_id' are global variables declared in the messages.html, which contains the ids of the users.
     $.get('/api/messages/' + sender_id + '/' + receiver_id, function (data) {
-        console.log(data);
         if (data.length !== 0) {
             for (var i = 0; i < data.length; i++) {
                 console.log(data[i]);
@@ -70,6 +69,7 @@ function getUsers(senderId, callback) {
         if (userState !== JSON.stringify(data)) {
             userState = JSON.stringify(data);
             const doc = data.reduce((res, user) => {
+                console.log(user);
                 if (user.id === senderId) {
                     return res
                 } else {
