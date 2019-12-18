9238b87bf4f4447089b0b6c0bd5531c1d69f3ca9
gabriel <gbp@ic.ufal.br>
2019-12-18 01:15:46 -0300

    Deploy

--- chatApp/settings/_base.py
+++ chatApp/settings/_base.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,6 +116,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, '../static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+
+# Whitenoise config
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CACHES = {
     'default': {
