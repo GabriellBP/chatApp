12176b643a3f10f44bf2cc02eae0a862310978f0
gabriel <gbp@ic.ufal.br>
2021-01-25 11:25:34 -0300

    revert commit 856bd88626143b706fd4cb78377e4f0564c3d491

--- chat/views.py
+++ chat/views.py
@@ -25,10 +25,7 @@ def user_list(request, pk=None):
         if pk:  # If PrimaryKey (id) of the user is specified in the url
             users = User.objects.filter(id=pk)  # Select only that particular user
         else:
-            if request.user.is_superuser:
-                users = User.objects.all().order_by('username').reverse()  # Else get all user list
-            else:
-                return JsonResponse({'error': "Not Authenticated"}, status=401)
+            users = User.objects.all().order_by('username').reverse()  # Else get all user list
         serializer = UserSerializer(users, many=True, context={'request': request})
         return JsonResponse(serializer.data, safe=False)  # Return serialized data
     elif request.method == 'POST':
